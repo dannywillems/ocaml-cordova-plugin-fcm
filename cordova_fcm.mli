4122855ef5cc6cfa7302e7996d1e7c5947e3bc6f
Danny Willems <contact@danny-willems.be>
2016-10-12 18:58:32 +0200

    Add get functions for some common key in Data

--- cordova_fcm.mli
+++ cordova_fcm.mli
@@ -4,17 +4,44 @@ type error = string
 
 module Data :
   sig
+    (** The type representing the data object received with each notification *)
     type t = Ojs.t
 
+    (** [was_tapped data] returns [true] if the application was in background
+        and the user taps the notification. If the application was in
+        foreground, the value is [false].
+     *)
     val was_tapped : t -> bool
     [@@js.get]
 
+    (** The ID of the application. For FCM, it's the project number.
+        NOTE: A value is set only if the application was in background.
+     *)
+    val from : t -> string option
+    [@@js.get]
+
+    val collapse_key : t -> string option
+    [@@js.get "collapse_key"]
+
+    (** The topic message ID when GCM has successfully received the request and
+        will attempt to deliver to all subscribed devices.
+        NOTE: A value is set only if the application was in background.
+     *)
+    val google_message_id : t -> string option
+    [@@js.get "google.message_id"]
+
     [@@@js.stop]
+    (** [get data key] returns the value of [data.key] as an [Ojs.t] type *)
     val get : t -> string -> Ojs.t
+
+    (** [get data key] returns the value of [data.key] as a [string] *)
+    val get_str : t -> string -> string
     [@@@js.start]
 
     [@@@js.implem
       let get data str = Ojs.get data str
+
+      let get_str data str = Ojs.string_of_js (Ojs.get data str)
     ]
   end
 
