8120bc81b3ba7580a4a37906f3699dd4cdc42d03
Danny Willems <contact@danny-willems.be>
2016-10-13 11:46:08 +0200

    Add documentation and wrapper get functions to built-in OCaml types

--- cordova_fcm.mli
+++ cordova_fcm.mli
@@ -11,46 +11,80 @@ module Data :
         and the user taps the notification. If the application was in
         foreground, the value is [false].
      *)
-    val was_tapped : t -> bool
+    val was_tapped          : t -> bool
     [@@js.get]
 
     (** The ID of the application. For FCM, it's the project number.
         NOTE: A value is set only if the application was in background.
      *)
-    val from : t -> string option
+    val from                : t -> string option
     [@@js.get]
 
-    val collapse_key : t -> string option
+    (** Seems to be always the application ID.
+        NOTE: A value is set only if the application was in background.
+     *)
+    val collapse_key        : t -> string option
     [@@js.get "collapse_key"]
 
     (** The topic message ID when GCM has successfully received the request and
         will attempt to deliver to all subscribed devices.
         NOTE: A value is set only if the application was in background.
      *)
-    val google_message_id : t -> string option
+    val google_message_id   : t -> string option
     [@@js.get "google.message_id"]
 
+    (** The following section defines functions to get data sent to FCM server in
+        the payload 'data'.
+     *)
     [@@@js.stop]
     (** [get data key] returns the value of [data.key] as an [Ojs.t] type *)
-    val get : t -> string -> Ojs.t
+    val get                 : t -> string -> Ojs.t
 
-    (** [get data key] returns the value of [data.key] as a [string] *)
-    val get_str : t -> string -> string
+    (** [get_string data key] returns the value of [data.key] as a string *)
+    val get_string          : t -> string -> string
+
+    (** [get_int data key] returns the value of [data.key] as an integer *)
+    val get_int             : t -> string -> int
+
+    (** [get_bool data key] returns the value of [data.key] as a boolean *)
+    val get_bool            : t -> string -> bool
+
+    (** [get_int64 data key] returns the value of [data.key] as an int64 type *)
+    val get_int64           : t -> string -> int64
     [@@@js.start]
 
     [@@@js.implem
-      let get data str = Ojs.get data str
+      let get data key        = Ojs.get data key
 
-      let get_str data str = Ojs.string_of_js (Ojs.get data str)
+      let get_string data key = Ojs.string_of_js (get data key)
+
+      let get_int data key    = Ojs.int_of_js (get data key)
+
+      let get_bool data key   = Ojs.bool_of_js (get data key)
+
+      let get_int64 data key  = Int64.of_string (get_string data key)
     ]
   end
 
+(** [get_token success_callback error_callback] gets a token from FCM.
+    If success, the callback [success_callback] is called and the token is given
+    in the first parameter.
+    If an error occured, the callback [error_callback] is called and the
+    error description is given as a string in the first parameter.
+ *)
 val get_token :
   (token -> unit) ->
   (error -> unit) ->
   unit
 [@@js.global "FCMPlugin.getToken"]
 
+(** [subscribe_to_topic topic success_callback error_callback] subscribes to the
+    topic [topic].
+    If success, the callback [success_callback] is called and a message is given
+    in the first parameter.
+    If an error occured, the callback [error_callback] is called and the
+    error description is given as a string in the first parameter.
+ *)
 val subscribe_to_topic :
   topic ->
   (string -> unit) ->
@@ -58,6 +92,13 @@ val subscribe_to_topic :
   unit
 [@@js.global "FCMPlugin.subscribeToTopic"]
 
+(** [unsubscribe_to_topic topic success_callback error_callback] unsubscribes to
+    the topic [topic].
+    If success, the callback [success_callback] is called and a message is given
+    in the first parameter.
+    If an error occured, the callback [error_callback] is called and the
+    error description is given as a string in the first parameter.
+ *)
 val unsubscribe_from_topic :
   topic ->
   (string -> unit) ->
@@ -65,6 +106,19 @@ val unsubscribe_from_topic :
   unit
 [@@js.global "FCMPlugin.unsubscribeFromTopic"]
 
+(** [on_notification callback_on_notification success_registering
+    error_registering] registers the callback [callback_on_notification] which
+    will be executed when the device will receive a notification. A [Data.t]
+    value is given in first parameter which contains keys sent by FCM. You can
+    get any key values by using the appropriate [Data.get] functions. Wrappers
+    for built-in OCaml types like get_string, get_bool, get_int, get_int64 are
+    defined to avoid using a cast function.
+
+    [success_registered] will be executed if the onNotification callback is
+    successfully registered.
+    [error_registered] will be executed if the onNotification callback failed to
+    register.
+ *)
 val on_notification :
   (Data.t -> unit) ->
   (string -> unit) ->
